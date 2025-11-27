a223d756f68715d39b86a417b66cc32957128d02
danielacvmelo <danielacvmelo@gmail.com>
2025-11-27 20:52:23 -0300

    refactor: change entity IDs to UUID

--- academia/src/main/java/com/projeto/academia/repository/EquipamentoRepository.java
+++ academia/src/main/java/com/projeto/academia/repository/EquipamentoRepository.java
@@ -4,6 +4,8 @@ import com.projeto.academia.model.Equipamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface EquipamentoRepository extends JpaRepository<Equipamento, Long> {
+public interface EquipamentoRepository extends JpaRepository<Equipamento, UUID> {
 }
--- academia/src/main/java/com/projeto/academia/repository/InstrutorRepository.java
+++ academia/src/main/java/com/projeto/academia/repository/InstrutorRepository.java
@@ -4,6 +4,9 @@ import com.projeto.academia.model.Instrutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {
+public interface InstrutorRepository extends JpaRepository<Instrutor, UUID> {
+    boolean existsByCref(String cref);
 }
